8ab201c82c7f3d17b0f411cf9e5371c050407df4
Kristian G Hammond <77787608+Earth2Kristian@users.noreply.github.com>
2025-05-23 14:51:27 +0100

    Adding buttons such as sin, cos, tan, pi, and the brackets

--- script.js
+++ script.js
@@ -18,9 +18,23 @@ function calculate(){
 
     try{
 
+        // Handling finding a power of a number
         display.value = display.value.replace(/(\d+)\^(\d+)/g, "Math.pow($1,$2)");
 
+        // Handling finding a square root of a number
         display.value = display.value.replace(/√(\d+(\.\d+)?)/g, "Math.sqrt($1)");
+
+        // Handling the sin, cos and tan functions
+        display.value = display.value.replace(/sin\(([^)]+)\)/g, "Math.sin($1 * Math.PI / 180)"); // sin
+
+        display.value = display.value.replace(/cos\(([^)]+)\)/g, "Math.cos($1 * Math.PI / 180)"); // cos
+
+        display.value = display.value.replace(/tan\(([^)]+)\)/g, "Math.tan($1 * Math.PI / 180)"); // tan
+
+        // Handling PI
+        display.value = display.value.replace(/π/g, "Math.PI");
+
+
 
         display.value = eval(display.value);
     }
